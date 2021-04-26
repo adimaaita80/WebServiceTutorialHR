5317af11b24685acd687f53c1370ed10fc16d681
Adi Maaita <adi.maaita80@gmail.com>
2021-04-26 17:36:54 +0300

    Completed GetDepartmentsFromDB

--- WebServiceTutorialHR/WebServiceHR.asmx.cs
+++ WebServiceTutorialHR/WebServiceHR.asmx.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace WebServiceTutorialHR
 {
@@ -16,11 +19,43 @@ namespace WebServiceTutorialHR
     // [System.Web.Script.Services.ScriptService]
     public class WebServiceHR : System.Web.Services.WebService
     {
+        DataTable tblDepartments = new DataTable();
 
         [WebMethod]
         public string HelloWorld()
         {
-            return "Hello World";
+            return "We are testing our first web service.";
+        }
+
+        [WebMethod]
+        public string GetDepartments()
+        {
+            tblDepartments.Columns.Add("DepartmentId");
+            tblDepartments.Columns.Add("DepartmentName");
+
+            tblDepartments.Rows.Add(1, "HR");
+            tblDepartments.Rows.Add(2, "Finance");
+            tblDepartments.Rows.Add(3, "Projects");
+
+            return JsonConvert.SerializeObject(tblDepartments);
+        }
+
+        [WebMethod]
+        public string GetDepartmentsFromDB()
+        {
+            string connectionString = @"server = DESKTOP-BKKL4I9\SQLEXPRESS ; database = HRDB ; integrated security = SSPI";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand command = new SqlCommand("Select * from Departments", connection);
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+            DataTable tblDatabaseDepartments = new DataTable();
+
+            dataAdapter.Fill(tblDatabaseDepartments);
+
+            return JsonConvert.SerializeObject(tblDatabaseDepartments);
         }
     }
 }
